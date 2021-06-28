de3eaf220b2a74fd0047318eb2f60617d11bac31
Maximillian Arruda <marruda@opentext.com>
2021-06-28 01:47:44 -0300

    Finalizando dia11

---
+++ dia11/src/main/java/noventaenovediasdejava/dia11/JogadorJokenpoRoundingRobin.java
@@ -0,0 +1,25 @@
+package noventaenovediasdejava.dia11;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class JogadorJokenpoRoundingRobin
+    implements JogadorJokenpo {
+    private final LinkedList<MovimentoJokenpo> movimentos;
+
+    public JogadorJokenpoRoundingRobin() {
+        this.movimentos = Arrays
+            .stream(MovimentoJokenpo.values())
+            .collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    @Override
+    public MovimentoJokenpo jogar() {
+        synchronized (this){
+            final var movimento = this.movimentos.poll();
+            this.movimentos.add(movimento);
+            return movimento;
+        }
+    }
+}
