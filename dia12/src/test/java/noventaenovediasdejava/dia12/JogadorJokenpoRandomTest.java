41702a0f9950598beff294b54bfd95e7cbec8bcb
Maximillian Arruda <marruda@opentext.com>
2021-06-29 02:33:39 -0300

    Finalizando dia12

---
+++ dia12/src/test/java/noventaenovediasdejava/dia12/JogadorJokenpoRandomTest.java
@@ -0,0 +1,35 @@
+package noventaenovediasdejava.dia12;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JogadorJokenpoRandomTest {
+    @Test
+    void testarJogar() {
+
+        JogadorJokenpo jogador = new JogadorJokenpoRandom();
+
+        final var movimentosExecutados =
+            IntStream.range(0, 10)
+                .boxed()
+                .map(tentativa -> {
+                    final var movimento = jogador.jogar();
+                    System.out.println("%s = %s".formatted(tentativa, movimento));
+                    return movimento;
+                })
+                .collect(Collectors.toList());
+
+        Assertions.assertEquals(
+            10,
+            movimentosExecutados
+                .stream()
+                .filter(Objects::nonNull)
+                .count(),
+            "total de movimentos executados fora do esperado"
+        );
+    }
+}
